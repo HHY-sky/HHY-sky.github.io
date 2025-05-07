368bb6fc43756221b43d65d9939f5b0f8197a085
HHY-sky <2568591476@qq.com>
2025-05-07 18:58:34 +0800

    Update tag_categories.js

--- src/data/tag_categories.js
+++ src/data/tag_categories.js
@@ -1,7 +1,13 @@
 define({
 	tagCategories: {
-		"type": {
-			"description": "type of the paper"
+		"methods": {
+			"description": "methods of the paper" 
+		},
+		"benchmarks": {
+			"description": "benchmarks of the paper" 
+		},
+		"classes": {
+			"description": "classifications of the paper" 
 		}
 	}
-});
+})
